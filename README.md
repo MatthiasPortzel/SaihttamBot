d6cef1fe5cc6dc61afff19907a4fb6eca5f8f069
Matthias <MatthiasSaihttam1@gmail.com>
2018-12-23 15:36:22 -0800

    Remove 0.1.0 code
    Remove KA account linking and other KA stuff and other commands no one ever used.

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # SaihttamBot
 Matthias's custom Discord bot.
 
-Originally based on Blaze's [Tucker](https://github.com/TuckerBot/Tucker2) code, since heavily modified.
-
 To install locally (for whatever reason):
 1. Clone this repository.
 2. Make sure Node.js and npm are installed.
-3. Install Discord.js with `npm install discord.js --save`
-4. Run `npm i hydrabolt/discord.js` to install the most recent version of Discord.js
-5. Create a `token.json` file with your token, surrounded by quotes.
-6. Run `node bot.js` to start.
+3. Run `npm intall` in this directory to install Discord.js
+4. Create a `passwords.json` file with `{ "discord": "YOUR_TOKEN_HERE" }`
+5. Run `node bot.js` to start.
--- bot.js
+++ bot.js
@@ -1,24 +1,16 @@
 /**
-    Saihttam, Matthias's Discord bot. Loosly based on Blaze's Tucker code.
-*/
-
-/* MODULES */
-
-//jshint esversion:6
+    Saihttam, Matthias's Discord bot.
+**/
 
 // Global dependencies
-const request = require('request');
-const querystring = require('querystring');
-var j = request.jar();
-
 const passwords = require('./passwords.json');
-const btoa = require("btoa");
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
 const Discord = require('discord.js');
 const Client = new Discord.Client();
 
-const con = mysql.createConnection({
+// I have a MySql server set up for Saihttam, but it's not currently being used
+/*const con = mysql.createConnection({
   host: "localhost",
   user: "SaihttamBot",
   password: passwords.mysql,
@@ -26,102 +18,11 @@ const con = mysql.createConnection({
 });
 
 con.connect();
+*/
 
 const prefix = "./";
 
 var runOnMessage = [];
-
-//Login to Khan Academy
-var fkey;
-var KArequest;
-
-var settings = {
-    method: "POST",
-    form: {
-        identifier: "SaihttamBot",
-        password: passwords.khanacademy,
-    },
-    url: "https://www.khanacademy.org/login",
-    jar: j,
-};
-request(settings, function (error, response, body) {
-    if (response && response.statusCode === 200) {
-        fkey = j.getCookies("https://www.khanacademy.org").find(c => c.key === "fkey").value;
-
-        KArequest = request.defaults({"baseUrl": "https://www.khanacademy.org", jar: j, headers: { "X-KA-FKEY": fkey }});
-
-        console.log("Logged in to Khan Academy.");
-
-        Client.login(passwords.discord).catch(console.error);
-    }else {
-        console.log(error || response);
-    }
-});
-
-var pending_links = [];
-
-function replyCheckLoop () {
-    KArequest("/api/internal/user/profile?username=SaihttamBot&projection={\"countBrandNewNotifications\":1}", function (e, r, d) {
-        d = JSON.parse(d);
-
-        console.log("Tick, notifs: ", d.countBrandNewNotifications);
-        //If there's a notif
-        if (d.countBrandNewNotifications > 0) {
-            //Check the replies to any pending links.
-            for (var i = 0; i < pending_links.length; i++) {
-
-                //Sort by recent--we want the for loop to hit the oldest first
-                KArequest(`/api/internal/discussions/${pending_links[i].kaEncryptedId}/replies?sort=2&projection=[{"normal":{"key":1,"content":1,"authorKaid":1,"authorNickname":1}}]&link_index=${i}`, function (error, response, data) {
-                    data = JSON.parse(data);
-
-                    if (response && !error && response.statusCode === 200) {
-                        var obj = pending_links[parseInt(querystring.parse(response.request.uri.query).link_index)];
-
-                        for (var j = 0; j < data.length; j++) {
-
-                            var reply_data = data[j];
-
-                            if (reply_data.content.includes(obj.verifyToken)) {
-                                //Save a link into the database with the discord_id and KAID -
-                                con.query(`INSERT INTO user_links (discord_id, kaid) VALUES ('${obj.author.id}', '${reply_data.authorKaid}')`, function (error, results, fields) {
-                                    if (error) throw error;
-
-                                    //Send message to the user stating the comment that was made and providing instructions on how to remove it +
-                                    obj.author.send(`I've seen a comment from KA user` +
-                                        ` ${reply_data.authorNickname} (https://khanacademy.org/profile/${reply_data.authorKaid}) with your reply code, and an account link has been successfully created.`);
-                                    obj.author.send("If this is the wrong user, or you want to link a different account, you can remove this link by issuing `./linkkaaccount remove`");
-
-                                    //Kill the timout
-                                    clearTimeout(obj.deleteTimer);
-
-                                    //Remove the pending link from the database +
-                                    pending_links.splice(obj.index, 1);
-
-                                    //Delete the comment
-                                    KArequest.delete(`/api/internal/feedback/${obj.kaEncryptedId}`, function (e, r, d) {
-                                        if (r.statusCode !== 200) console.error(e, r, d);
-                                    });
-                                });
-                            }
-                        }
-                    }
-                });
-            }
-
-            KArequest.post("/api/internal/user/notifications/clear_brand_new", function (e, r, d) {
-                if (!r || r.statusCode !== 200) {
-                    console.log(e, r);
-                }else {
-                    console.log("Read notifs")
-                }
-            });
-        }
-
-        if (pending_links.length > 0) {
-            replyCheckLoop();
-        }
-    });
-}
 
 function addReacts (message, codePoint, numReacts) {
     if (codePoint - 127462 >= numReacts) {
@@ -138,177 +39,13 @@ var commands = {
         message.channel.send(content);
     },
 
-    kanameavailable(message, content) {
-        if (!content) {
-            message.channel.send("Must pass in a KA username.");
-        } else {
-            if (/[^\w.]/.test(content)) {
-                message.channel.send("Your \"username\" contains an invalid character.");
-                return;
-            }
-            var url = "/api/internal/signup/check-username?username=" + content;
-            KArequest(url, function(error, response, body) {
-                if (response) {
-                    if (!error && response.statusCode === 200) {
-                        var data = JSON.parse(body);
-                        if (data) {
-                            message.channel.send("This username is available! :confetti_ball:");
-                        } else {
-                            message.channel.send("That username isn't available. :frowning:");
-                        }
-                    } else {
-                        if (response) {
-                            message.channel.send("\uD83D\uDCE3 I got an error while parsing this command. Please try again. Status Code **" + response.statusCode + "**");
-                        }
-                    }
-                }
-
-            });
-        }
-    },
-
-    linkkaaccount(message, content) {
-        var args = content.split(" ");
-
-        var discord_id = message.author.id;
-        if (args.length && args[0] === "cancel") {
-            var index = pending_links.findIndex(l => l.author.id === message.author.id);
-            if (index === -1) {
-                message.channel.send("You didn't have a pending link request.");
-            }else {
-                var obj = pending_links.splice(index, 1)[0];
-
-                KArequest.delete(`/api/internal/feedback/${obj.kaEncryptedId}`,
-                    (e, r, d) => { if (r.statusCode !== 200) console.log(e, r, d); }
-                );
-
-                message.channel.send("Your pending request has been deleted.");
-            }
-        }else if (args.length && args[0] === "remove") {
-            con.query(`DELETE from user_links WHERE discord_id=${message.author.id}`, function (error, results, fields) {
-                if (error) throw error;
-                if (results.affectedRows) {
-                    message.channel.send("KA account link removed successfully.");
-                }else {
-                    message.channel.send("You don't appear to have a KA account linked with your Discord account.")
-                }
-            });
-        }else if (!args.length || !args[0]) {
-            con.query(`SELECT kaid FROM user_links WHERE discord_id="${discord_id}"`, function (error, results, fields) {
-                if (error) return console.error(error);
-
-                if (results.length) {
-                    message.channel.send(`Your Discord account is already linked to the following KA account: https://khanacademy.org/profile/${results[0].kaid}`);
-                    return;
-                }
-
-                //Also, check if there's an already pending link.
-                if (pending_links.find(l => l.discord_id === message.author.id)) {
-                    message.channel.send("You already have a pending link request. Use `./linkkaaccount cancel` to cancel it.");
-                    return;
-                }
-
-                message.channel.send("I'll PM you instructions on linking your KA account. One second.");
-
-                var settings = {
-                    url: "/api/internal/discussions/scratchpad/5118767575367680/comments",
-                    method: "POST",
-                    json: {
-                        text: `If you are Discord user ${message.author.tag}, please comment below with the code I sent you.`,
-                        topic_slug :"computer-programming",
-                    },
-                };
-                KArequest(settings, function(error, response, data) {
-                    if (response) {
-                        if (!error && response.statusCode === 200) {
-                            var time = Date.now();
-                            //Generate a verification token
-                            var verifyToken = btoa(`${discord_id};time;${btoa(Math.round(Math.random()*10000000))}`);
-                            //Save link, time, discord_id, and verify_code
-                            var link = `https://khanacademy.org${data.focusUrl}?qa_expand_key=${data.expandKey}`;
-                            var kaEncryptedId = data.key;
-
-                            var obj = {
-                                "time": time,
-                                "author": message.author,
-                                "index": null,
-                                "kaEncryptedId": kaEncryptedId,
-                                "verifyToken": verifyToken
-                            };
-
-                            obj.index = pending_links.push(obj) - 1;
-
-                            //Send user a message with a link to ther location and their verify code
-                            message.author.send("In order to continue linking this Discord account with a KA account, please copy the following verification token.");
-                            message.author.send('\u2015'.repeat(20) + "\n" + obj.verifyToken + "\n" + '\u2015'.repeat(20));
-                            message.author.send("Please reply to the linked comment that I've created with your token.\n" + link);
-                            message.author.send("If you do nothing after 3 minutes, or after you comment with your token, the KA comment will be deleted to protect the privacy of your Discord account.");
-                            message.author.send("If you'd like to cancel this request you can reply with `./linkkaaccount cancel`.");
-
-                            //Start a timeout for 3 minutes to delete the comment
-                            obj.deleteTimer = setTimeout(function () {
-                                KArequest.delete(`/api/internal/feedback/${kaEncryptedId}`, function(e, r, d) {
-                                    if (e || r.statusCode !== 200) {
-                                        console.error(e, r, d);
-                                    }else {
-                                        pending_links.splice(obj.index, 1);
-
-                                        message.author.send("Since you haven't replied within 3 minutes, I've deleted my comment and canceled your request.");
-                                        message.author.send("If you'd like to link your Discord and KA accounts, you can restart the process by replying with `./linkkaaccount`.");
-                                    }
-                                });
-
-
-                            }, 1000 * 60 * 3);
-
-                            //If it's not already running, start an interval to check notifications
-                            if (pending_links.length === 1) {
-                                replyCheckLoop();
-                            }
-                        } else {
-                            console.log(error, response, data);
-                        }
-                    }
-                });
-            });
-        }else {
-            message.channel.send("Sorry, argument not recognized. Use `./linkkaaccount` in order to link your KA and Discord accounts.")
-        }
-    },
-
-    bitcoin(message) {
-        message.channel.startTyping();
-        request("https://blockchain.info/tobtc?currency=USD&value=1", function (error, response, body) {
-            if (response) {
-                if (!error && response.statusCode === 200) {
-                    var data = JSON.parse(body);
-                    message.channel.send(`1 Bitcoin is currently worth $${(1/data).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}.`);
-                } else {
-                    if (response) {
-                        message.channel.send("\uD83D\uDCE3 I got an error. Please try again later. Status Code **" + response.statusCode + "**");
-                    }
-                }
-                message.channel.stopTyping();
-            }
-        });
-    },
-
     help(message) {
-        message.channel.send('Current Commands:\n```' + prefix + Object.keys(this).join(`, ${prefix}`) + '```');
-    },
-
-    lmgtfy(message, content) {
-        message.channel.send('Let me google that for you: <http://lmgtfy.com/?q=' + encodeURIComponent(content) + '>');
+        message.channel.send('Current Commands:\n```' + prefix + Object.keys(this).join(`,\n${prefix}`) + '```');
     },
 
     about(message) {
-        message.channel.send("This is a bot developed by Matthias, based off of Blaze's Tucker" +
-            " framework, but modified heavily. It mainly serves to help Matthias and to service the OurJSEditor server. " +
+        message.channel.send("This is a bot developed by Matthias." +
             "It's open source here: https://github.com/MatthiasSaihttam/SaihttamBot.");
-    },
-
-    ping(message) {
-        message.channel.send('pong');
     },
 
     math(message, content) {
@@ -424,14 +161,6 @@ Client.on('message', (message) => {
         }
 
         var content = message.content;
-        if (content === "e$Fishfake" || content.toLowerCase() === "e$fakefishfake") {
-            message.channel.send(
-                ":fishing_pole_and_fish:  |  **" + message.author.username + ", you caught:**:paperclip:! Paid :yen: 0 for casting."
-            ).catch(console.error);
-        }
-        if (content === "c!ping") {
-            message.channel.send("pong");
-        }
 
         verifyCarets(message);
 
@@ -455,17 +184,13 @@ Client.on('ready', () => {
     // Client.user.setAvatar('./images/image.png')
 })
 
-Client.on('messageDelete', (message) => {
-    console.log(`${message.author.tag} said ${message.content} in #${message.channel.name}, before it was deleted`);
-})
-
 Client.on('messageUpdate', (oldMess, newMess) => {
     verifyCarets(newMess);
 })
 
 function exitHandler(options, err) {
     Client.destroy();
-    con.end();
+    // con.end();
     process.exit();
 }
 
